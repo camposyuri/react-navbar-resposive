a4e2fcef3356881a6d7da65034964e305b2de2b3
camposyuri <yurinapoleao@gmail.com>
2020-10-19 14:19:25 -0300

    [ADD] - Adding Navbar JSX

--- react-navbar/src/components/Navbar/index.js
+++ react-navbar/src/components/Navbar/index.js
@@ -3,7 +3,32 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <h1>Navbar</h1>
+      <Nav>
+        <NavLink to="/">
+          <h1>Logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+
+          <NavLink to="/srervices" activeStyle>
+            Services
+          </NavLink>
+
+          <NavLink to="/contact-us" activeStyle>
+            Contact Us
+          </NavLink>
+
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
     </>
   );
 };
